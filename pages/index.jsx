b938046ddf98fe16de450c5a5b25cb718af5b40c
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-03 12:30:37 +0530

    app documnent and error page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
 import Home from '../components/Home';
+// import { useEffect } from 'react';
 
 const home = () => {
+  // useEffect(() => {
+  //   throw new Error('Failed');
+  // The error will be displayed on the production end
+  // }, []);
   return <Home />;
 };
 
